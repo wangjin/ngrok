f3aeff7d5c29b2ec5838fbba1fe1886a4b58f315
wangjin <wangjin-252@hotmail.com>
2017-12-20 10:02:26 +0800

    修改监听端口

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/bin/ngrokd -domain="ngrok.jsictiot.com" -httpAddr=":8088" -httpsAddr=":8089"
+/usr/local/bin/ngrokd -domain="ngrok.jsictiot.com" -httpAddr=":80" -httpsAddr=":443"
